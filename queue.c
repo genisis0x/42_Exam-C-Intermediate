c055404d0affbef7728c24d5ef53d00a93e7cc4d
Manmeet <manietdavv@gmail.com>
2019-03-18 18:05:30 -0700

    Done

---
+++ queue.c
@@ -0,0 +1,98 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   queue.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maparmar <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/18 17:26:21 by maparmar          #+#    #+#             */
+/*   Updated: 2019/03/18 18:04:57 by maparmar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+#include <stdlib.h>
+
+struct  s_node
+{
+    void            *content;
+    struct s_node   *next;
+};
+struct  s_queue
+{
+    struct s_node   *first;
+    struct s_node   *last;
+};
+
+struct s_queue *init(void)
+{
+	struct s_queue *q = (struct s_queue *)malloc(sizeof(struct s_queue));
+	if (!q)
+		return NULL;
+	q->first = NULL;
+	q->last = NULL;
+	return(q);
+}
+void enqueue(struct s_queue *queue, void *content)
+{
+	struct s_node *new = (struct s_node *)malloc(sizeof(struct s_node));
+	new->content = content;
+	new->next = NULL;
+	
+	if (!queue->first)
+		queue->first = new;
+	if (queue->last)
+		queue->last->next = new;
+	queue->last = new;
+}
+
+void *dequeue(struct s_queue *queue)
+{
+	void *value = NULL;
+	struct s_node *temp = queue->first;
+	if (queue->first)
+	{
+		value = queue->first->content;
+		queue->first = queue->first->next;
+		queue->last = (queue->last) ? queue->first : NULL;
+		free(temp);
+	}
+	return (value);
+}
+void *peek(struct s_queue *queue)
+{
+	return (queue->first ? queue->first->content : NULL);
+}
+
+int isEmpty(struct s_queue *queue)
+{
+	return (queue->first ? 0 : 1);
+}
+
+#include <stdio.h>
+int main(void)
+{
+    struct s_queue  *q = init();
+    char            *content[][1] = {
+      "1",
+      "2",
+      "3",
+      "4",
+      "5"
+    };
+    for (int i = 0; i < 5; i++)
+    {
+        enqueue(q, *content[i]);
+        printf("Content : %s\n", peek(q));
+        printf("Empty   : %s\n", (isEmpty(q) ? "yes" : "no"));
+    }
+    for (int i = 5; i > 0; i--)
+    {
+        dequeue(q);
+        printf("Content : %s\n", peek(q));
+        printf("Empty   : %s\n", (isEmpty(q) ? "yes" : "no"));
+    }
+    return (0);
+}
+
