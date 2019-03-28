b2b54407099a3a1460978cbad781897a13108676
Manmeet <manietdavv@gmail.com>
2019-03-28 01:14:13 -0700

    Added more level0 Questions

---
+++ count_of_2.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   count_of_2.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maparmar <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/21 01:37:39 by maparmar          #+#    #+#             */
+/*   Updated: 2019/03/28 01:12:53 by maparmar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdio.h>
+void  ft_count_nbr(int nb, int *count)
+{
+	if (nb >=10)
+	{
+		ft_count_nbr( nb / 10, count);
+		ft_count_nbr(nb % 10, count);
+	}
+	if (nb == 2)
+		*count += 1;
+}
+
+int count_of_2(int n)
+{
+	int result = 0;
+	int i = 1;
+	int count = 0;
+
+	while ( ++i <= n)
+	{
+		ft_count_nbr(i, &count);
+	}
+	return (count);
+}
+
+int main (int ac , char **av)
+{
+	int res;
+	res = count_of_2(atoi(av[1]));
+	printf("%i", res);
+}
---
+++ find_pivot.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   find_pivot.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maparmar <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/22 22:39:48 by maparmar          #+#    #+#             */
+/*   Updated: 2019/03/22 23:12:12 by maparmar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int find_pivot(int *arr, int n)
+{
+	int total_sum = 0;
+	int remain_sum = 0;
+	int pivot = -1;
+	for (int i = 0; i < n; i++)
+		total_sum += arr[i];
+	for (int i = 0; i < n; i++)
+	{
+		total_sum -= arr[i];
+		if (remain_sum == total_sum)
+			return (i);
+		remain_sum += arr[i];
+	}
+	return(-1);
+}
+
+/**********
+ TEST_MAIN
+ *********/
+#include <stdio.h>
+int main (void)
+{
+	int arr[] = {1, 0, 1};
+	printf("The pivot is: %d",find_pivot(arr, 3));
+	return 0;
+}
