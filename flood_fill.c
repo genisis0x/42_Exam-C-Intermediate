040125f585cacc26cb0cd0b691dae6681d7519fb
Manmeet <manietdavv@gmail.com>
2019-04-23 17:52:26 -0700

    flood_file

---
+++ flood_fill.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   flood_fill.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: exam <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/23 09:19:45 by exam              #+#    #+#             */
+/*   Updated: 2019/04/23 17:43:25 by maparmar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "t_point.h"
+
+void  flood_fill(char **tab, t_point size, t_point begin)
+{
+	char c;
+	c = tab[begin.y][begin.x];
+	
+	tab[begin.y][begin.x] = 'F';
+	
+	begin.y++;
+	if(begin.y < size.y && tab[begin.y][begin.x] == c)
+		flood_fill(tab, size, begin);
+
+	begin.y--;
+	begin.y--;
+	if(begin.y >= 0 && tab[begin.y][begin.x] == c)
+		flood_fill(tab, size, begin);
+	
+	begin.y++;
+	begin.x++;
+	if(begin.x < size.x && tab[begin.y][begin.x] == c)
+		flood_fill(tab, size, begin);
+	
+	begin.x--;
+	begin.x--;
+	if(begin.x >= 0 && tab[begin.y][begin.x] == c)
+		flood_fill(tab, size, begin);
+}
+
