3a69e217cdcc2ee6221b166d0d80d95af33e5161
Manmeet <manietdavv@gmail.com>
2019-04-24 02:06:42 -0700

    Added 2nd solution to is_looping with main

--- level1/is_looping_2.c
+++ level1/is_looping_2.c
@@ -6,7 +6,7 @@
 /*   By: maparmar <maparmar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/24 01:48:46 by maparmar          #+#    #+#             */
-/*   Updated: 2019/04/24 01:57:17 by maparmar         ###   ########.fr       */
+/*   Updated: 2019/04/24 02:06:24 by maparmar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,7 +50,12 @@ int main(void)
 	l->next->next->next = new (4);
 	l->next->next->next->next = l;
 	printf("The value of is_looping is %d\n", is_looping(l));
-	l->next->next->next->next = new (5);
-	printf("The value of is_looping is %d\n", is_looping(l));
+
+	struct s_node *a = new (1);
+	a->next = new (2);
+	a->next->next = new (3);
+	a->next->next->next = new (4);
+	a->next->next->next->next = new (5);
+	printf("The value of is_looping is %d\n", is_looping(a));
 	return 0;
 }
