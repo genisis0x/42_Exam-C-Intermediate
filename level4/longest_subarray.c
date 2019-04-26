1f9735a4ada84773f0971f9dd18f35a35defbc91
Manmeet Singh Parmar <manietdavv@gmail.com>
2019-04-26 08:11:46 -0700

    Create longest_subarray.c

---
+++ level4/longest_subarray.c
@@ -0,0 +1,45 @@
+#include <string.h>
+#include <stdlib.h>
+
+int odd_num(char c)
+{
+    return (!((c - '0') % 2));
+}
+
+char* longest_subarray(char* arr)
+{
+    int len = strlen(arr);
+    int sum = 0, start_index, max_len = -1;
+    int i, j;
+    for(i = 0; i < len -1; i++)
+    {
+        sum = (odd_num(arr[i]) == 1) ? -1 : 1;
+        for(j = i + 1; j < len; j++)
+        {
+            odd_num(arr[j]) == 1 ? (sum += -1) : (sum += 1);
+
+            if(sum == 0 && max_len < j - i + 1)
+            {
+                max_len = j - i + 1;
+                start_index = i;
+            }
+        }
+    }
+    if (max_len == -1)
+    {
+        return NULL;
+    }
+    char *res = (char *)malloc(sizeof(char) * (max_len + 1));
+    for(i = start_index, j = 0; i < max_len; i++)
+    {
+        res[j] = arr[i];
+    }
+    return res;
+}
+
+#include <stdio.h>
+int main (int ac, char **av)
+{
+    printf("%s", longest_subarray(av[1]));
+    return 0;
+}
