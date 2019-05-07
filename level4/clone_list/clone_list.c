851212af1bf542cdd2b70f6536ae6ab88ee2beac
Manmeet singh Parmar <maparmar@e1z3r3p10.42.us.org>
2019-05-07 00:04:03 -0700

    Clone list

--- level4/clone_list/clone_list.c
+++ level4/clone_list/clone_list.c
@@ -1,105 +1,100 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   clone_list.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: maparmar <maparmar@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/04/29 15:58:53 by maparmar          #+#    #+#             */
-/*   Updated: 2019/04/29 17:58:50 by maparmar         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <stdlib.h>
 
-struct s_node {
-        int           data;
-        struct s_node *next;
-        struct s_node *other;
-    };
+typedef struct s_node {
+    int           data;
+    struct s_node *next;
+    struct s_node *other;
+} Node;
 
-struct s_node *head = NULL;
+Node* clone_list(Node* node) {
 
-struct s_node *new(int data)
-{
-	struct s_node *n = (struct s_node *)malloc(sizeof(struct s_node));
-	n->data = data;
-	n->next = NULL;
-    n->other = NULL;
-    return n;
-}
-
-struct s_node *clone_list(struct s_node *node)
-{
-    if(node == NULL)
+    if (!node)
         return 0;
-    struct s_node *n = node;
-    
-    while(n)
-    {
-    	if(head == NULL)
-    		head = new(n->data);
-		else
-			head->next = new(n->data);
-		n = n->next;
-	}
-    
-    n = node;
-    struct s_node *clone = head;
-    while(n)
-    {
-    	n->next = clone;
-    	clone = clone->next;
-    	if(n)
-    		n = n->next; 
+
+    Node* original = node;
+    Node* temp;
+
+    // Clone list in-between original
+    while (original) {
+        temp = original->next;
+        original->next = malloc(sizeof(Node));
+        original->next->data = original->data;
+        original->next->other = 0;
+        original->next->next = temp;
+        original = temp;
     }
-    
-    n = node;
-    clone = head;
-    while(n)
-    {
-    	clone->other = n;
-    	clone = clone->next;
-    	n =n->next;
+
+    original = node;
+
+    // Set other pointers for cloned list
+    while (original) {
+        if (original->other)
+            original->next->other = original->other->next;
+        if (original->next)
+            original = original->next->next;
+        else
+            break;
     }
-    n = node;
-    clone = head;
-    while(clone)
-    {
-    	if(clone->other->other->next) clone->other = clone->other->other->next;
-    	clone = clone->next;
+
+    original = node;
+    Node* copy = node->next;
+    Node* copyHead = copy;
+
+    while (original && copy) {
+        if (original->next)
+            original->next = original->next->next;
+        if (copy->next)
+            copy->next = copy->next->next;
+        original = original->next;
+        copy = copy->next;
     }
-    return head;
+    return copyHead;
 }
 
-
-
-/********************
-  	 TEST MAIN
-*******************/
+/************
+  Test Main
+************/
 
 #include <stdio.h>
-int main(void)
-{
-    struct s_node *a = new(1);
-    a->next = new(2);
-    a->next->next = new(3);
-    a->next->next->next = new(4);
+Node* b(int v) {
+    Node* new = malloc(sizeof(Node));
+    new->data = v;
+    new->next = 0;
+    new->other = 0;
+    return new;
+}
+int main() {
+    Node* a = b(1);
+    a->next = b(2);
+    a->next->next = b(3);
+    a->next->next->next = b(4);
+    a->next->next->next->next = b(5);
+    a->next->next->next->next->next = b(6);
+    a->next->next->next->next->next->next = b(7);
 
-    a->other = a->next->next;
-    a->next->other = a;
-    a->next->next->other = a->next->next;
-    a->next->next->next->other = a->next;
+    Node* a2 = a->next;
+    Node* a3 = a2->next;
+    Node* a4 = a3->next;
+    Node* a5 = a4->next;
+    Node* a6 = a5->next;
+    Node* a7 = a6->next;
 
-    struct s_node *res = clone_list(a);
-    while(res)
-    {
-        printf("%d| ", res->other->data);
-        printf("%d->\n", res->data);
-        res = res->next;
+    a2->other = a7;
+    a3->other = a;
+    a5->other = a5;
+    a7->other = a4;
+
+    Node* clone = clone_list(a);
+    Node* original = a;
+    puts("Original:");
+    while (original) {
+        printf("Val: %d | Other: %d\n", original->data, original->other ? original->other->data : -1);
+        original = original->next;
     }
-    // res = clone_list(0);
-    // if(res == 0)
-    //     printf("Mother Fucker!!\n");
+    puts("Clone:");
+    while (clone) {
+        printf("Val: %d | Other: %d\n", clone->data, clone->other ? clone->other->data : -1);
+        clone = clone->next;
+    }
     return 0;
 }
