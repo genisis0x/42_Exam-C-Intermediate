682314d505331a21df26aaec8cce938e5fc9f8fd
Manmeet Singh Parmar <manietdavv@gmail.com>
2019-05-09 08:56:33 -0700

    Update count_alpha.c

--- level1/count_alpha/count_alpha.c
+++ level1/count_alpha/count_alpha.c
@@ -9,81 +9,73 @@
 /*   Updated: 2019/04/01 01:37:16 by maparmar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include <stdio.h>
-#include <unistd.h>
-
-int valid_char_small(char c)
+int is_cap_alpha(char c)
 {
-	return (c >= 'a'  && c <= 'z');
+    return (c >= 'A' && c <= 'Z');
 }
 
-int valid_char_Cap(char c)
+int is_sma_alpha(char c)
 {
-	return (c >= 'A'  && c <= 'Z');
+    return (c >= 'a' && c <= 'z');
 }
 
-void ft_count_alpha(char *s)
+void count_alp(char *s)
 {
-	int char_arr[26] = {0};
-	int flag = 1;
-	int i = 0;
-	while(s[i])
-	{
-		if(valid_char_small(s[i]))
-		{
-			char_arr[(int)(s[i] - 'a')] += 1;
-		}
-		if (valid_char_Cap(s[i]))
-		{
-			char_arr[(int)(s[i] - 'A')] += 1;
-		}
-		i++;
-	}
-	i = 0;
-	while(s[i])
-	{
-		if (valid_char_Cap(s[i]) || valid_char_small(s[i]))
-		{
-			if(valid_char_Cap(s[i]) && char_arr[(int)(s[i] - 'A')] > 0)
-			{
-				if (flag == 1)
-				{
-					printf("%d%c", char_arr[(int)(s[i] - 'A')], s[i] + 32);
-					char_arr[(int)(s[i] - 'A')] = 0;
-					flag = 0;
-				}
-				else
-				{
-					printf(", %d%c", char_arr[(int)(s[i] - 'A')], s[i] + 32);
-					char_arr[(int)(s[i] - 'A')] = 0;
-				}
-			}
-			if(valid_char_small(s[i]) && char_arr[(int)(s[i] - 'a')] > 0)
-			{
-				if (flag == 1)
-				{
-					printf("%d%c", char_arr[(int)(s[i] - 'a')], s[i]);
-					char_arr[(int)(s[i] - 'a')] = 0;
-					flag = 0;
-				}
-				else
-				{
-					printf(", %d%c", char_arr[(int)(s[i] - 'a')], s[i]);
-					char_arr[(int)(s[i] - 'a')] = 0;
-				}
-
-			}
-		}
-		i++;
-	}
+    int arr[26] = {0};
+    int i = 0, flag = 0;
+    while(s[i])
+    {
+        if(is_cap_alpha(s[i]))
+            arr[(int)s[i] - 'A'] += 1;
+        else if(is_sma_alpha(s[i]))
+            arr[(int)s[i] - 'a'] += 1;
+        i++;
+    }
+    i = 0;
+    while(s[i])
+    {
+        if(is_cap_alpha(s[i]) || is_sma_alpha(s[i]))
+        {
+            if(is_cap_alpha(s[i]) && arr[(int)s[i] - 'A'] > 0)
+            {
+                if(flag == 0)
+                {
+                    printf("%d%c", arr[(int)s[i] - 'A'], s[i] + 32);
+                    flag++;
+                    arr[(int)s[i] - 'A'] = 0;
+                }
+                else
+                {
+                    printf(", %d%c", arr[(int)s[i] - 'A'], s[i] + 32);
+                    arr[(int)s[i] - 'A'] = 0;
+                }
+            }
+            else if(is_sma_alpha(s[i]) && arr[(int)s[i] - 'a'] > 0)
+            {
+                if(flag == 0)
+                {
+                    printf("%d%c", arr[(int)s[i] - 'a'], s[i]);
+                    flag++;
+                    arr[(int)s[i] - 'a'] = 0;
+                }
+                else
+                {
+                    printf(", %d%c", arr[(int)s[i] - 'a'], s[i]);
+                    arr[(int)s[i] - 'a'] = 0;
+                }
+            }
+            
+        }
+        i++;
+    }
 }
-int main (int ac, char **av)
+
+int main(int ac, char **av)
 {
-	if (ac == 2)
-	{
-		ft_count_alpha(av[1]);
-	}
-	printf("\n");
-	return (0);
+    if(ac == 2)
+    {
+        count_alp(av[1]);
+    }
+    printf("\n");
 }
