01fe3c7cd4ab78279b42f72a47e3e84b2a88c8c5
Manmeet Singh <manmeetsingh@Manmeets-MacBook-Pro.local>
2019-05-12 02:31:37 -0700

    Updated level4

--- level4/range_comb/range_comb.c
+++ level4/range_comb/range_comb.c
@@ -6,7 +6,7 @@
 /*   By: manmeetsingh <manmeetsingh@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/12 02:19:35 by manmeetsing       #+#    #+#             */
-/*   Updated: 2019/05/12 02:29:26 by manmeetsing      ###   ########.fr       */
+/*   Updated: 2019/05/12 02:31:33 by manmeetsing      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ int    **range_comb(int n)
         res[0][i] = i;
     
     for(int i = 1; i < fac(n); i++)
-        res[i] = resgenerate(res[i - 1], 0, n -1);
+        res[i] = generate(res[i - 1], 0, n -1);
     
     return res;
 }
