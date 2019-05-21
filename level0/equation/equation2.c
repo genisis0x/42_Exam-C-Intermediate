681429903512b1bccd2763e3c853ba95665f8385
Manmeet Singh Parmar <manietdavv@gmail.com>
2019-05-20 20:41:00 -0700

    Create equation2.c

---
+++ level0/equation/equation2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+void    equation(int n)
+{
+    int A = 0, B, C;
+    {
+        while( 11 * A <= n && A < 10)
+        {
+            int rem = n - (11 * A);
+            B = rem % 10;
+            C = rem / 10;
+            if(B < 10 && C < 10)
+                printf("A = %i, B = %i, C = %i\n", A, B, C);
+            A++;
+        }
+    }    
+}
+
+
+// int main ()
+// {
+//     int n;
+//     scanf("%d", &n);
+//     equation(n);
+//     return 0;
+// }
