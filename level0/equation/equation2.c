11ab50c3c14fe045955b889401bd0700236d8944
Manmeet Singh Parmar <manietdavv@gmail.com>
2019-05-21 20:21:26 -0700

    Update equation2.c

--- level0/equation/equation2.c
+++ level0/equation/equation2.c
@@ -1,19 +1,19 @@
+/****************************************** Passed in Exam *************************************************/ 
+
 #include <stdio.h>
 void    equation(int n)
 {
+    for(int A = 0, B, C; 11 * A <= n && A < 10; A++)
     {
-        for (int A = 0, B, C; 11 * A <= n && A < 10; A++)
-        {
-            int rem = n - (11 * A);
-            B = rem % 10, C = rem / 10;
-            if(B < 10 && C < 10)
-                printf("A = %i, B = %i, C = %i\n", A, B, C);
-        }
-    }    
+        int rem = n - (11 * A);
+        B = rem % 10;
+        C = rem / 10;
+        if(B < 10 && C < 10)
+            printf("A = %i, B = %i, C = %i\n", A, B, C);
+    }
 }
 
-
-// int main ()
+// int main()
 // {
 //     int n;
 //     scanf("%d", &n);
